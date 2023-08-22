e7c2166e14333924b0c02a44e60d6400be393d7c
aashraf09 <asad15112007@gmail.com>
2023-08-23 00:55:12 +0500

    fixing onScroll animation

--- index.js
+++ index.js
@@ -3,12 +3,14 @@ function reveal() {
   
     for (var i = 0; i < reveals.length; i++) {
       var windowHeight = window.innerHeight;
+      console.log(windowHeight);
       var elementTop = reveals[i].getBoundingClientRect().top;
-      var elementVisible = 150;
+      var elementVisible = 10;
   
-      if (elementTop < windowHeight - elementVisible) {
+      if (elementTop <= windowHeight - elementVisible) {
         reveals[i].classList.add("animate");
-      } else {
+      } 
+      else {
         reveals[i].classList.remove("animate");
       }
     }
